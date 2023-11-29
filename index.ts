c4deec2d1e1635b8c89eaf84c3ae2ff524f9e8cf
Tam <Tam@TAM-DESKTOP>
2023-11-29 10:33:17 +0700

    Cài đặt PUG và kết nối database

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log(1)
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});

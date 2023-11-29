2245e2ae9cdb159b58b3bde4150108cfc96ec1aa
Tam <Tam@TAM-DESKTOP>
2023-11-29 11:48:48 +0700

    Làm trang Chủ đề bài hát (bên client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -3,12 +3,12 @@ import Topic from "../../models/topic.model";
 
 // [GET] /topics/
 export const topics = async (req: Request, res: Response)=>{
-    const topic = await Topic.find({
+    const topics = await Topic.find({
         deleted: false
     });
-    console.log(topic);
 
     res.render("client/pages/topics/index.pug",{
-        pageTitle: "Chủ đề bài hát"
+        pageTitle: "Chủ đề bài hát",
+        topics: topics
     });
 }

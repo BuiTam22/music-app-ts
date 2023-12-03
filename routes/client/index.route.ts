de02e31035c47c3e5efc0237048470c7ea364ba6
Tam <Tam@TAM-DESKTOP>
2023-12-03 14:42:40 +0700

    Làm trang Bài hát yêu thích

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,12 +1,14 @@
 import { Express } from "express";
 import { topicRoutes } from "./topic.route";
 import { songRoutes } from "./song.route";
-
+import { favoriteSongRouters } from "./favorite.route";
 const clientRoutes = (app: Express) => {
 
     app.use("/topics", topicRoutes);
     
     app.use(`/songs`, songRoutes);
+
+    app.use('/favorite-songs', favoriteSongRouters);
 
 };
 

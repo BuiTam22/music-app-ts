a608b310cbecd56baf3c0a36c7fab0a2a5855020
Tam <Tam@TAM-DESKTOP>
2023-12-05 09:29:42 +0700

    Tìm kiếm hiển thị suggest (gợi ý)

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -4,24 +4,26 @@ import Singer from "../../models/singer.model";
 import { convertToSlug } from "../../helpers/convertToSlug";
 
 
-// [GET] /search/result
+// [GET] /search/:type
 export const result = async(req: Request, res: Response) =>{
+    const type: string = req.params.type;
     const keyword: string = `${req.query.keyword}`;
 
     let newSongs = [];
   
     if(keyword) {
       const keywordRegex = new RegExp(keyword, "i");
-  
       // Tạo ra slug không dấu, có thêm dấu - ngăn cách
       const stringSlug = convertToSlug(keyword);
       const stringSlugRegex = new RegExp(stringSlug, "i");
-  
+      console.log(keyword);
+      console.log(keywordRegex);
+      console.log(stringSlugRegex);
       const songs = await Song.find({
         $or: [
           { title: keywordRegex },
           { slug: stringSlugRegex }
-        ]
+        ] 
       });
 
       for(let i = 0; i<songs.length; i++){
@@ -29,14 +31,37 @@ export const result = async(req: Request, res: Response) =>{
             _id: songs[i].singerId
           });
     
-          songs[i]["infoSinger"] = infoSinger;
+          // songs[i]["infoSinger"] = infoSinger;
+          newSongs.push({
+            id: songs[i].id,
+            title: songs[i].title,
+            avatar: songs[i].avatar,
+            like: songs[i].like,
+            slug: songs[i].slug,
+            infoSinger: {
+              fullName: infoSinger.fullName
+            }
+          });
       }
-      newSongs=songs;
+      // newSongs=songs;
     }
 
-    res.render("client/pages/search/result", {
-        pageTitle: `Kết quả: ${keyword}`,
-        keyword: keyword,
-        songs: newSongs
-    });
+    switch (type) {
+      case "result":
+        res.render("client/pages/search/result", {
+          pageTitle: `Kết quả: ${keyword}`,
+          keyword: keyword,
+          songs: newSongs
+        });
+        break;
+        case "suggest":
+          res.json({
+            code: 200,
+            message: "Thành công!",
+            songs: newSongs
+          });
+          break;
+      default:
+        break;
+    }
 }

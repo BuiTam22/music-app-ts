374d86a70ce53922e70c970c66f3dbe4aeb91066
Tam <Tam@TAM-DESKTOP>
2023-12-06 11:45:07 +0700

    Layout chung bên admin

--- index.ts
+++ index.ts
@@ -1,23 +1,37 @@
-import express, {Express} from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import path from "path";
 import * as database from "./config/database";
+
+import adminRoutes from "./routes/admin/index.route";
 import clientRoutes from "./routes/client/index.route";
+import { systemConfig } from "./config/config";
 
 dotenv.config();
 
 database.connect();
-
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
-
 app.use(express.static("public"));
-
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+// Admin Routes
+adminRoutes(app);
 
 // Client Routes
 clientRoutes(app);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
